3bc0d4a87d3f0d1c5b82d7fa45507a75df496916
Artur Gawron <artur@gawron.me>
2020-01-30 00:43:27 +0100

    Add toggle player after roll 1

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     } else {
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0;
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = none;
+
+
     }
 
 
