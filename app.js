308f680de5d40ee0513fe2315735131eeb0c8f35
Artur Gawron <artur@gawron.me>
2020-01-31 21:47:15 +0100

    Add feature "double six lose"
    after roll 6 2 times in a row lose all points!

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, lastDice;
 var diceDOM = document.querySelector('.dice');
 
 initGame();
@@ -9,12 +9,18 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         var dice = Math.floor(Math.random() * 6) + 1;
         diceDOM.style.display = 'block';
         diceDOM.src = 'img/dice-' + dice + '.png';
-        if (dice !== 1) {
+        if (dice === 6 && lastDice === 6) {
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = '0';
+            nextPlayer();
+        } else if (dice !== 1) {
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
             nextPlayer();
         }
+
+        lastDice = dice;
     }
 });
 
@@ -25,7 +31,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
         diceDOM.style.display = 'none';
 
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
             diceDOM.style.display = 'none';
             gamePlaying = false;
@@ -44,6 +50,7 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
     diceDOM.style.display = 'none';
+    lastDice = 0;
 };
 
 
